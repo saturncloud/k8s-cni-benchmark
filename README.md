580986e4870dbb38c7e1ed955acfc9e90babef31
Ben Perry <bhperry94@gmail.com>
2021-03-03 13:03:45 -0600

    Generalize scripts and templates

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # Benchmarking tools for Kubernetes CNIs
 
+
+Updates made to run these benchmarks more generically, specifically for running on Saturn clusters
+
+Example usage:
+
+```sh
+./bench.sh --protocols "TCP HTTP" --context mycluster --time 60 --tag aws-cni
+./bench.sh --protocols "TCP HTTP" --context mycluster2 --time 60 --tag calico
+...
+```
+
+
+
 The goal of this repository is to publish the scripts and tools used in the benchmark conducted by Alexis Ducastel (Twitter : [@infraBuilder](https://twitter.com/infraBuilder), Medium : [Alexis Ducastel](https://medium.com/@infrabuilder)).
 
 An article has been published on Medium with the first results published on November 2018 : https://itnext.io/benchmark-results-of-kubernetes-network-plugins-cni-over-10gbit-s-network-36475925a560
-
-**NB** : The scripts are not generic for now.
 
 Specials thanks to [@tgraf__](https://twitter.com/tgraf__) and [@martyns](https://twitter.com/martyns) for the monitoring fix applied before to push this repo.
--- bench.sh
+++ bench.sh
@@ -1,210 +1,314 @@
 #!/bin/bash
 
+DIR=$(dirname $0)
 BENCH_CYCLE="3"
 IPERFTIME="120"
-
-function log { echo $(date "+%Y-%m-%d %H:%M:%S") $@; }
+DEFAULT_PROTOCOLS=( TCP UDP HTTP FTP SCP )
+function log {
+	if [ "$OUTPUT_FILE" ]; then
+		echo $@ | tee -a $OUTPUT_FILE
+	else
+		echo $@
+	fi
+}
+function log_ts { log $(date "+%Y-%m-%d %H:%M:%S") $@; }
 function info { log INFO "$@"; }
 function warning { log WARNING "$@"; }
 function error { log ERROR "$@"; }
 function fatal { log FATAL "$@"; exit 2; }
 
+function usage {
+	echo "Usage: ./bench.sh [options]
+
+Options:
+	-c, --context: Kubeconfig context to run benchmark in (default: <current>)
+	-i, --iterations: Number of benchmark iterations per protocol (default: $BENCH_CYCLE)
+	-n, --node, --nodes: Nodes to run servers/benchmarks on. 2 required. (default: Detect nodes without the NoSchedule taint)
+	-p, --protocol, --protocols: Protocols to benchmark (default: \"${DEFAULT_PROTOCOLS[@]}\")
+	-t, --time: Time in seconds to run iperf benchmark for TCP and UDP tests (default: $IPERFTIME)
+	--tag: Tag for results text file (default: <context>)
+	-h, --help: This ;P"
+}
+
+while [ "$1" ]; do
+	case $1 in
+		-c | --context )
+			shift
+			CONTEXT=$1
+			;;
+		-i | --iterations )
+			shift
+			BENCH_CYCLE=$1
+			;;
+		-n | --node | --nodes )
+			shift
+			NODES=( ${NODES[@]} $1)
+			;;
+		-p | --protocol | --protocols )
+			shift
+			PROTOCOLS=( ${PROTOCOLS[@]} $1 )
+			;;
+		-t | --time )
+			shift
+			IPERFTIME=$1
+			;;
+		--tag )
+			shift
+			TAG=$1
+			;;
+		-h | --help )
+			usage
+			exit 0
+			;;
+		* )
+			fatal "Unknown argument \"$1\""
+			;;
+	esac
+	shift
+done
+
+if [ ! "$CONTEXT" ]; then
+	CONTEXT=$(kubectl config current-context 2>/dev/null || true)
+fi
+
+if [ "$PROTOCOLS" ]; then
+	for PROTO in ${PROTOCOLS[@]}; do
+		if [[ ! " ${PROTOCOLS[@]} " =~ " ${PROTO} " ]]; then
+			fatal "Unkown protocol: $PROTO"
+		fi
+	done
+else
+	PROTOCOLS=( ${DEFAULT_PROTOCOLS[@]} )
+fi
+
+mkdir -p ${DIR}/results/
+TAG=${TAG:-${CONTEXT:-default}}
+OUTPUT_FILE=$DIR/results/bench_${TAG}_$(date +%s).txt
+
+
 #==============================================================================
 # Pre-flight checks
 #==============================================================================
 
-cd $(dirname $0)
-
-source lib/ssh.sh
-
-export KUBE_CONFIG="$(pwd)/kubeconfig"
-
 function bench_kubectl {
-	kubectl run --restart=Never --rm \
-		--overrides='{"apiVersion":"v1","spec":{"nodeSelector":{"kubernetes.io/hostname":"s03"}}}' $@
+	kubectl --context=${CONTEXT} run --restart=Never --rm \
+		--labels="app=k8s-cni-benchmark,run=bench" \
+		--overrides='{"apiVersion":"v1","spec":{"nodeSelector":{"kubernetes.io/hostname":"'${NODES[1]}'"}}}' $@
 }
 
-function mon_start {
-	TIME=$(date +%s)
-}
-function mon_end {
-	TIME=$(( $(date +%s) - $TIME ))
-	echo -e "$(lssh s03 ./stats.sh $TIME 2>/dev/null)\t$(lssh s02 ./stats.sh $TIME 2>/dev/null)"
+function apply_yml {
+	local YAML_FILE=$1
+	local NODE=$2
+
+	cat $YAML_FILE | sed "s/s02/${NODE}/g" | kubectl --context=${CONTEXT} apply -f - > /dev/null
 }
 
 SUMMARY=""
 
-
-info "Waiting for systems to be idle ..."
-sleep 60
-
-info "Getting idle consumption (1min poll)"
-mon_start
-sleep 60
-SUMMARY="$SUMMARY\t$(mon_end)"
+if [ ! "${NODES}" ]; then
+	NODES=( $(kubectl --context=${CONTEXT} get nodes -o json | jq -r '.items[] | select(.spec.taints == null or (.spec.taints | select(.[].effect != "NoSchedule"))) | .metadata.name') )
+	if (( ${#NODES[@]} < 2 )); then
+		fatal "Not enough schedulable nodes found"
+	fi
+elif (( ${#NODES[@]} < 2 )); then
+	fatal "At least 2 nodes are requiured"
+fi
 
 #==============================================================================
 # Iperf
 #==============================================================================
-info "Starting iperf3 server"
-kubectl apply -f kubernetes/server-iperf3.yml > /dev/null
+iperf() {
+	info "Starting iperf3 server"
+	apply_yml kubernetes/server-iperf3.yml ${NODES[0]}
 
-info "Waiting for pod to be alive"
-while true; do kubectl get pod|grep iperf-srv |grep Running >/dev/null && break; sleep 1; done
+	info "Waiting for pod to be alive"
+	while true; do kubectl --context=${CONTEXT} get pod|grep iperf-srv |grep Running >/dev/null && break; sleep 1; done
 
-# Retrieving Pod IP address
-IP=$(kubectl get pod/iperf-srv -o jsonpath='{.status.podIP}')
-info "Server iperf3 is listening on $IP"
+	# Retrieving Pod IP address
+	IP=$(kubectl --context=${CONTEXT} get pod/iperf-srv -o jsonpath='{.status.podIP}')
+	info "Server iperf3 is listening on $IP"
+}
 
 #===[ TCP ]=======
-info "Launching benchmark for TCP"
-TOT_TCP=0
-for i in $(seq 1 $BENCH_CYCLE)
-do
-	mon_start
-	RES_TCP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client -- iperf3 -c $IP -O $(( $IPERFTIME / 10 )) -f m -t $IPERFTIME 2>/dev/null \
-		| grep receiver| awk '{print $7}')
-	[ "$i" = "1" ] && MON="$(mon_end)"
-	TOT_TCP=$(( $TOT_TCP + $RES_TCP ))
-	info "TCP $i/$BENCH_CYCLE : $RES_TCP Mbit/s"
-	sleep 1
-done
-RES_TCP=$(( $TOT_TCP / $BENCH_CYCLE ))
-info "TCP result $RES_TCP Mbit/s"
-SUMMARY="$SUMMARY\t$RES_TCP\t$MON"
+TCP() {
+	info "Launching benchmark for TCP"
+	TOT_TCP=0
+	for i in $(seq 1 $BENCH_CYCLE)
+	do
+		RES_TCP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client -- iperf3 -c $IP -O $(( $IPERFTIME / 10 )) -f m -t $IPERFTIME 2>/dev/null \
+			| grep receiver| awk '{print $7}')
+		TOT_TCP=$(( $TOT_TCP + $RES_TCP )) || error "Bad value RES_TCP: $RES_TCP"
+		info "TCP $i/$BENCH_CYCLE : $RES_TCP Mbit/s"
+		sleep 1
+	done
+	RES_TCP=$(( $TOT_TCP / $BENCH_CYCLE ))
+	info "TCP result $RES_TCP Mbit/s"
+	SUMMARY="$SUMMARY\t$RES_TCP"
+}
 
 #===[ UDP ]=======
-info "Launching benchmark for UDP"
-TOT_UDP=0
-TOT_JIT=0
-TOT_DROP=0
-for i in $(seq 1 $BENCH_CYCLE)
-do
-	mon_start
-	read RES_UDP JITTER_UDP DROP_UDP <<< $(bench_kubectl bench -it --image=infrabuilder/netbench:client -- iperf3 -u -b 0 -c $IP -O $(( $IPERFTIME / 10 )) -w 256K -f m -t $IPERFTIME 2>/dev/null \
-		| grep receiver| sed 's/.* sec//'|awk '{print $3" "$5" "$8}' | tr -d "()%")
-	[ "$i" = "1" ] && MON="$(mon_end)"
-	TOT_UDP=$(( $TOT_UDP + $RES_UDP ))
-	PART_JIT=$(printf "%.3f" $JITTER_UDP| tr -d "."| sed 's/^0*//')
-	TOT_JIT=$(( $TOT_JIT + $PART_JIT ))
-	TOT_DROP=$(( $TOT_DROP + $( printf "%.0f" $DROP_UDP) ))
-	info "UDP $i/$BENCH_CYCLE : $RES_UDP Mbit/s ${PART_JIT}us jitter ${DROP_UDP}% drop"
-	sleep 1
-done
-RES_UDP=$(( $TOT_UDP / $BENCH_CYCLE ))
-JIT_UDP=$(( $TOT_JIT / $BENCH_CYCLE ))
-DROP_UDP=$(( $TOT_DROP / $BENCH_CYCLE ))
-info "UDP result $RES_UDP Mbit/s ${JIT_UDP}us jitter ${DROP_UDP}% drop"
-SUMMARY="$SUMMARY\t$RES_UDP\t$JIT_UDP\t$DROP_UDP\t$MON"
+UDP() {
+	info "Launching benchmark for UDP"
+	TOT_UDP=0
+	TOT_JIT=0
+	TOT_DROP=0
+	for i in $(seq 1 $BENCH_CYCLE)
+	do
+		read RES_UDP JITTER_UDP DROP_UDP <<< $(bench_kubectl bench -it --image=infrabuilder/netbench:client -- iperf3 -u -b 0 -c $IP -O $(( $IPERFTIME / 10 )) -w 256K -f m -t $IPERFTIME 2>/dev/null \
+			| grep receiver| sed 's/.* sec//'|awk '{print $3" "$5" "$8}' | tr -d "()%")
+		TOT_UDP=$(( $TOT_UDP + $RES_UDP ))
+		PART_JIT=$(printf "%.3f" $JITTER_UDP| tr -d "."| sed 's/^0*//')
+		TOT_JIT=$(( $TOT_JIT + $PART_JIT ))
+		TOT_DROP=$(( $TOT_DROP + $( printf "%.0f" $DROP_UDP) ))
+		info "UDP $i/$BENCH_CYCLE : $RES_UDP Mbit/s ${PART_JIT}us jitter ${DROP_UDP}% drop"
+		sleep 1
+	done
+	RES_UDP=$(( $TOT_UDP / $BENCH_CYCLE ))
+	JIT_UDP=$(( $TOT_JIT / $BENCH_CYCLE ))
+	DROP_UDP=$(( $TOT_DROP / $BENCH_CYCLE ))
+	info "UDP result $RES_UDP Mbit/s ${JIT_UDP}us jitter ${DROP_UDP}% drop"
+	SUMMARY="$SUMMARY\t$RES_UDP\t$JIT_UDP\t$DROP_UDP"
+}
 
-info "Cleaning resources"
-kubectl delete -f kubernetes/server-iperf3.yml >/dev/null
+iperf-cleanup() {
+	info "Cleaning resources"
+	kubectl --context=${CONTEXT} delete -f kubernetes/server-iperf3.yml >/dev/null
+}
 
 #==============================================================================
 # HTTP
 #==============================================================================
-info "Starting HTTP server"
-kubectl apply -f kubernetes/server-http.yml >/dev/null
+HTTP() {
+	info "Starting HTTP server"
+	apply_yml kubernetes/server-http.yml ${NODES[0]}
 
-info "Waiting for pod to be alive"
-while true; do kubectl get pod|grep http-srv |grep Running >/dev/null && break; sleep 1; done
+	info "Waiting for pod to be alive"
+	while true; do kubectl --context=${CONTEXT} get pod|grep http-srv |grep Running >/dev/null && break; sleep 1; done
 
-IP=$(kubectl get pod/http-srv -o jsonpath='{.status.podIP}')
-info "Server HTTP is listening on $IP"
+	IP=$(kubectl --context=${CONTEXT} get pod/http-srv -o jsonpath='{.status.podIP}')
+	info "Server HTTP is listening on $IP"
 
-info "Launching benchmark for HTTP"
-TOT_HTTP=0
-for i in $(seq 1 $BENCH_CYCLE)
-do
-	mon_start
-	RES_HTTP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client \
-		-- curl -o /dev/null -skw "%{speed_download}" http://$IP/10G.dat 2>/dev/null| sed 's/\..*//' )
-	[ "$i" = "1" ] && MON="$(mon_end)"
-	TOT_HTTP=$(( $TOT_HTTP + RES_HTTP ))
-	info "HTTP $i/$BENCH_CYCLE : $(( $RES_HTTP * 8 / 1024/ 1024 )) Mbit/s"
-	sleep 1
-done
-RES_HTTP=$(( $TOT_HTTP * 8 / $BENCH_CYCLE / 1024 / 1024 ))
+	info "Launching benchmark for HTTP"
+	TOT_HTTP=0
+	for i in $(seq 1 $BENCH_CYCLE)
+	do
+		RES_HTTP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client \
+			-- curl -o /dev/null -skw "%{speed_download}" http://$IP/10G.dat 2>/dev/null| sed 's/\..*//' )
+		TOT_HTTP=$(( $TOT_HTTP + RES_HTTP ))
+		info "HTTP $i/$BENCH_CYCLE : $(( $RES_HTTP * 8 / 1024/ 1024 )) Mbit/s"
+		sleep 1
+	done
+	RES_HTTP=$(( $TOT_HTTP * 8 / $BENCH_CYCLE / 1024 / 1024 ))
 
-info "HTTP result $RES_HTTP Mbit/s"
-SUMMARY="$SUMMARY\t$RES_HTTP\t$MON"
+	info "HTTP result $RES_HTTP Mbit/s"
+	SUMMARY="$SUMMARY\t$RES_HTTP\t"
 
-info "Cleaning resources"
-kubectl delete -f kubernetes/server-http.yml >/dev/null
+	info "Cleaning resources"
+	kubectl --context=${CONTEXT} delete -f kubernetes/server-http.yml >/dev/null
+}
 
 
 #==============================================================================
 # FTP
 #==============================================================================
-info "Starting FTP server"
-kubectl apply -f kubernetes/server-ftp.yml >/dev/null
+FTP() {
+	info "Starting FTP server"
+	apply_yml kubernetes/server-ftp.yml ${NODES[0]}
 
-info "Waiting for pod to be alive"
-while true; do kubectl get pod|grep ftp-srv |grep Running >/dev/null && break; sleep 1; done
+	info "Waiting for pod to be alive"
+	while true; do kubectl --context=${CONTEXT} get pod|grep ftp-srv |grep Running >/dev/null && break; sleep 1; done
 
-IP=$(kubectl get pod/ftp-srv -o jsonpath='{.status.podIP}')
-info "Server FTP is listening on $IP"
+	IP=$(kubectl --context=${CONTEXT} get pod/ftp-srv -o jsonpath='{.status.podIP}')
+	info "Server FTP is listening on $IP"
 
-info "Launching benchmark for FTP with $BENCH_CYCLE cycles"
-TOT_FTP=0
-for i in $(seq 1 $BENCH_CYCLE)
-do
-	mon_start
-	RES_FTP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client \
-		-- curl -o /dev/null -skw "%{speed_download}" ftp://$IP/10G.dat 2>/dev/null| sed 's/\..*//' )
-	[ "$i" = "1" ] && MON="$(mon_end)"
-	TOT_FTP=$(( $TOT_FTP + RES_FTP ))
-	info "FTP $i/$BENCH_CYCLE : $(( $RES_FTP * 8 / 1024/ 1024 )) Mbit/s"
-	sleep 1
-done
-RES_FTP=$(( $TOT_FTP * 8 / $BENCH_CYCLE / 1024 / 1024 ))
+	info "Launching benchmark for FTP with $BENCH_CYCLE cycles"
+	TOT_FTP=0
+	for i in $(seq 1 $BENCH_CYCLE)
+	do
+		RES_FTP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client \
+			-- curl -o /dev/null -skw "%{speed_download}" ftp://$IP/10G.dat 2>/dev/null| sed 's/\..*//' )
+		TOT_FTP=$(( $TOT_FTP + RES_FTP ))
+		info "FTP $i/$BENCH_CYCLE : $(( $RES_FTP * 8 / 1024/ 1024 )) Mbit/s"
+		sleep 1
+	done
+	RES_FTP=$(( $TOT_FTP * 8 / $BENCH_CYCLE / 1024 / 1024 ))
 
-info "FTP result $RES_FTP Mbit/s"
-SUMMARY="$SUMMARY\t$RES_FTP\t$MON"
+	info "FTP result $RES_FTP Mbit/s"
+	SUMMARY="$SUMMARY\t$RES_FTP"
 
-info "Cleaning resources"
-kubectl delete -f kubernetes/server-ftp.yml >/dev/null
+	info "Cleaning resources"
+	kubectl --context=${CONTEXT} delete -f kubernetes/server-ftp.yml >/dev/null
+}
 
 
 #==============================================================================
 # SCP
 #==============================================================================
-info "Starting SCP server"
-kubectl apply -f kubernetes/server-ssh.yml >/dev/null
+SCP() {
+	info "Starting SCP server"
+	apply_yml kubernetes/server-ssh.yml ${NODES[0]}
 
-info "Waiting for pod to be alive"
-while true; do kubectl get pod|grep ssh-srv |grep Running >/dev/null && break; sleep 1; done
+	info "Waiting for pod to be alive"
+	while true; do kubectl --context=${CONTEXT} get pod|grep ssh-srv |grep Running >/dev/null && break; sleep 1; done
 
-IP=$(kubectl get pod/ssh-srv -o jsonpath='{.status.podIP}')
-info "Server SCP is listening on $IP"
+	IP=$(kubectl --context=${CONTEXT} get pod/ssh-srv -o jsonpath='{.status.podIP}')
+	info "Server SCP is listening on $IP"
 
-info "Launching benchmark for SCP with $BENCH_CYCLE cycles"
-TOT_SCP=0
-for i in $(seq 1 $BENCH_CYCLE)
-do
-	mon_start
-	RES_SCP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client \
-		-- sshpass -p root scp  -o UserKnownHostsFile=/dev/null \
-		-o StrictHostKeyChecking=no -v root@$IP:/root/10G.dat ./ 2>/dev/null\
-		| grep "Bytes per second" |sed -e 's/.*received //' -e 's/\..*$//' )
-	[ "$i" = "1" ] && MON="$(mon_end)"
-	TOT_SCP=$(( $TOT_SCP + RES_SCP ))
-	info "SCP $i/$BENCH_CYCLE : $(( $RES_SCP * 8 / 1024/ 1024 )) Mbit/s"
-	sleep 1
+	info "Launching benchmark for SCP with $BENCH_CYCLE cycles"
+	TOT_SCP=0
+	for i in $(seq 1 $BENCH_CYCLE)
+	do
+		RES_SCP=$(bench_kubectl bench -it --image=infrabuilder/netbench:client \
+			-- sshpass -p root scp  -o UserKnownHostsFile=/dev/null \
+			-o StrictHostKeyChecking=no -v root@$IP:/root/10G.dat ./ 2>/dev/null\
+			| grep "Bytes per second" |sed -e 's/.*received //' -e 's/\..*$//' )
+		TOT_SCP=$(( $TOT_SCP + RES_SCP ))
+		info "SCP $i/$BENCH_CYCLE : $(( $RES_SCP * 8 / 1024/ 1024 )) Mbit/s"
+		sleep 1
+	done
+	RES_SCP=$(( $TOT_SCP * 8 / $BENCH_CYCLE / 1024 / 1024 ))
+
+	info "SCP result $RES_SCP Mbit/s"
+	SUMMARY="$SUMMARY\t$RES_SCP"
+
+	info "Cleaning resources"
+	kubectl --context=${CONTEXT} delete -f kubernetes/server-ssh.yml >/dev/null
+}
+
+declare -A IPERF_PROTOS=(
+	[TCP]=""
+	[UDP]=""
+)
+
+for PROTO in ${!IPERF_PROTOS[@]}; do
+	if [[ " ${PROTOCOLS[@]} " =~ " $PROTO " ]]; then
+		IPERF_PROTOS[$PROTO]=true
+	fi
 done
-RES_SCP=$(( $TOT_SCP * 8 / $BENCH_CYCLE / 1024 / 1024 ))
 
-info "SCP result $RES_SCP Mbit/s"
-SUMMARY="$SUMMARY\t$RES_SCP\t$MON"
+IPERF_SETUP=""
+for PROTO in ${PROTOCOLS[@]}; do
+	if [ "${IPERF_PROTOS[$PROTO]}" ]; then
+		if [ ! "$IPERF_SETUP" ]; then
+			iperf
+			IPERF_SETUP=true
+		fi
 
-info "Cleaning resources"
-kubectl delete -f kubernetes/server-ssh.yml >/dev/null
+		$PROTO
+
+		IPERF_PROTOS[$PROTO]=""
+		if [ ! ${IPERF_PROTOS[@]} ]; then
+			iperf-cleanup
+		fi
+	else
+		$PROTO
+	fi
+done
 
 
 #==============================================================================
 # SUMMARY
 #==============================================================================
-echo "========================================================================="
-#echo -e "SUMMARY: $RES_TCP\t$RES_UDP\t$JIT_UDP\t$DROP_UDP\t$RES_HTTP\t$RES_FTP\t$RES_SCP"
-echo -e "SUMMARY: $SUMMARY"
-echo "========================================================================="
+log "========================================================================="
+log -e "SUMMARY: $SUMMARY"
+log "========================================================================="
